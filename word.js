964e8a8f7d5e4e445b8ea1454e775e075e289394
syeda zaidi <zaidisyeda03@gmail.com>
2019-08-23 14:03:31 -0700

    word object finished

--- word.js
+++ word.js
@@ -4,7 +4,24 @@ class Word {
     constructor(word) {
         this.lettersArr = [];
         for (var i =0; i < word.length; i++) {
-            
+            var letter = new Letter(word[i]);
+            this.lettersArr.push(letter);
         }
     }
-}
+
+        log = () => {
+            var wordLog = "";
+            for (var i = 0; i < this.lettersArr.length; i++) {
+                wordLog = wordLog + this.lettersArr[i] + " ";
+            };
+            console.log(wordLog + "\n----------\n");
+        };
+
+        userGuess = (userInput) => {
+            for (var i = 0; i < this.lettersArr.length; i++) {
+                this.lettersArr[i].checkGuess(userInput);
+            }
+        }
+    }
+
+module.exports = Word;
